a115643d5745a51bc24a7f0b92e39063411da807
wwwestin <westin.h@gmail.com>
2022-02-25 15:30:43 -0500

    completed code-along

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import NavBar from "./NavBar";
 import MoviesPage from "./MoviesPage";
 
 function App() {
-  const [movies, setMovies] = useState({
+  const [movies] = useState({
     1: { id: 1, title: "A River Runs Through It" },
     2: { id: 2, title: "Se7en" },
     3: { id: 3, title: "Inception" },
